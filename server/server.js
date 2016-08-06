b598d0a8c3e7fad2079f4faf1d482f430d543165
kleeut <klee.ut@gmail.com>
2016-08-07 09:32:32 +1000

    Changed capitalisation of port to try and get app running on Heroku

--- server/server.js
+++ server/server.js
@@ -9,7 +9,8 @@ app.use(express.static('server/public'));
 
 Routes(app, chuckFacts);
 
-var port = process.env.port || 8080;
+var port = process.env.PORT || 8081;
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 }) ;
