05e1e8bb58f87a16a7d9b336010a667de306a1aa
kleeut <klee.ut@gmail.com>
2016-08-07 18:30:13 +1000

    Added the add to slack button

--- server/public/bundle.js
+++ server/public/bundle.js
@@ -146,13 +146,22 @@
 	          ),
 	          _react2.default.createElement(
 	            'div',
-	            null,
+	            { className: 'col-sm-6' },
 	            _react2.default.createElement(
 	              'button',
 	              { className: 'btn btn-primary btn-lg', onClick: function onClick() {
 	                  (0, _FactGetter2.default)(store);
 	                } },
 	              'Random Chuck Fact'
+	            )
+	          ),
+	          _react2.default.createElement(
+	            'div',
+	            { className: 'col-sm-6' },
+	            _react2.default.createElement(
+	              'a',
+	              { href: 'https://slack.com/oauth/authorize?scope=incoming-webhook,commands&client_id=3358490050.67017692769' },
+	              _react2.default.createElement('img', { alt: 'Add to Slack', height: '40', width: '139', src: 'https://platform.slack-edge.com/img/add_to_slack.png', srcset: 'https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x' })
 	            )
 	          ),
 	          _react2.default.createElement(
